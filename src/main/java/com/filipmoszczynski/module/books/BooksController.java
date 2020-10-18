c6caa605bacc80c38f2bd4b59a905ec3539497ab
Filip M <filip.moszczynski@onet.pl>
2020-10-18 09:49:32 +0200

    >D3.3 - Controller - update

--- src/main/java/com/filipmoszczynski/module/books/BooksController.java
+++ src/main/java/com/filipmoszczynski/module/books/BooksController.java
@@ -3,6 +3,7 @@ package com.filipmoszczynski.module.books;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -26,7 +27,7 @@ public class BooksController {
 	}
 
 	@PostMapping("/api/books")
-	public BooksEntity newBooks(@RequestBody BooksEntity booksEntity) {
+	public BooksEntity newBooks(@Valid @RequestBody BooksEntity booksEntity) {
 		return booksRepository.saveAndFlush(booksEntity);
 	}
 
